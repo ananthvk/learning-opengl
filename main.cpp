571c5a0aab9baeb66bd8d7d21c1c2885db35ec7e
Ananth V <123456789987654321sr@gmail.com>
2023-04-10 13:02:03 +0530

    Wrote code to load shaders

--- main.cpp
+++ main.cpp
@@ -5,20 +5,24 @@
 // The vertex and fragment shaders
 // Vertex shader specifies how to process the vertices
 // while fragment shader gives the color for each pixel.
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}\0";
-
-const char *fragmentShaderSource = "#version 330 core\nout vec4 FragColor;\n\nvoid main()\n{\n    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n} \n";
+//const char *vertexShaderSource = "#version 330 core\n"
+//                                 "layout (location = 0) in vec3 aPos;\n"
+//                                 "void main()\n"
+//                                 "{\n"
+//                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//                                 "}\0";
+//
+//const char *fragmentShaderSource = "#version 330 core\nout vec4 FragColor;\n\nvoid main()\n{\n    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n} \n";
+char *vertexShaderSource = nullptr;
+char *fragmentShaderSource = nullptr;
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     // Function which is called each time the window is resized
     glViewport(0, 0, width, height);
 }
+
+#define BUFSIZE 10000
 
 void processInput(GLFWwindow *window)
 {
@@ -31,6 +35,24 @@ void processInput(GLFWwindow *window)
 
 int main()
 {
+
+    // No error handling or dynamic buffers for now
+    char buffer1[BUFSIZE];
+    char buffer2[BUFSIZE];
+    FILE *fp;
+    fp = fopen("main.vert", "r");
+    size_t sz = fread(buffer1, 1, BUFSIZE-1, fp);
+    buffer1[sz] = '\0';
+    fclose(fp);
+
+    fp = fopen("main.frag", "r");
+    sz = fread(buffer2, 1, BUFSIZE-1, fp);
+    buffer2[sz] = '\0';
+    fclose(fp);
+
+    vertexShaderSource = buffer1;
+    fragmentShaderSource = buffer2;
+
     // Initialize GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -130,19 +152,23 @@ int main()
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
 
 
     unsigned int VBO = 1;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
+    int nrAttributes;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
     // For wireframe mode
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -153,6 +179,10 @@ int main()
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
+        //for(int i = 0; i < 4*3; i++){
+        //    vertices[i] += 0.01;
+        //}
+        //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
