0f639d172749f4222d91a8d73be2e273c281658d
Ananth V <123456789987654321sr@gmail.com>
2023-04-12 22:20:31 +0530

    Multiple rotated cubes

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include "shader.h"
 
 float global_mix = 0.2f;
+float rotateSpeed = 1.0f;
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
@@ -35,6 +36,15 @@ void processInput(GLFWwindow *window)
     {
         global_mix = std::max(global_mix - 0.01f, 0.0f);
     }
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        rotateSpeed += 1;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        rotateSpeed -= 1;
+    }
 }
 
 unsigned int createVAO(const float *vertices, size_t vertices_length)
@@ -46,30 +56,30 @@ unsigned int createVAO(const float *vertices, size_t vertices_length)
     glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &old_EBO);
 
     // Create the VAO, and the buffers
-    unsigned int VAO = 1, VBO = 1;//, EBO = 1;
+    unsigned int VAO = 1, VBO = 1; //, EBO = 1;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    //glGenBuffers(1, &EBO);
-    // Bind the VAO and the buffers
+    // glGenBuffers(1, &EBO);
+    //  Bind the VAO and the buffers
     glBindVertexArray(VAO);
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     // Copy the arrays into the buffers
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_length, indices, GL_STATIC_DRAW);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_length, indices, GL_STATIC_DRAW);
     glBufferData(GL_ARRAY_BUFFER, vertices_length, vertices, GL_STATIC_DRAW);
 
     // Enable the attrib array to access the vertices
     // Vertices
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(0));
     // Colors
-    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
     // Textures
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
 
     // Enable the attribute pointers
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
-    //glEnableVertexAttribArray(2);
+    // glEnableVertexAttribArray(2);
 
     // Bind the VAO, VBO, and EBO to the ones before this function was called
     glBindVertexArray(old_VAO);
@@ -240,16 +250,25 @@ float vertices[] = {
     shader.set_int("boxTexture", 0);
     shader.set_int("smileyTexture", 1);
 
-    glEnable(GL_DEPTH_TEST);  
+    glEnable(GL_DEPTH_TEST);
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)};
 
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 
         float timeElapsed = glfwGetTime();
         shader.use();
@@ -263,8 +282,9 @@ float vertices[] = {
         // glActiveTexture(GL_TEXTURE0);
         // glBindTexture(GL_TEXTURE_2D, texture);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        //glm::mat4 model = glm::mat4(1.0f);
+        // model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        //model = glm::rotate(model, (float)(glfwGetTime() * rotateSpeed) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
         glm::mat4 view = glm::mat4(1.0f);
         // note that we're translating the scene in the reverse direction of where we want to move
@@ -273,8 +293,6 @@ float vertices[] = {
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-        unsigned int modelLocation = glGetUniformLocation(shader.getid(), "model");
-        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
 
         unsigned int viewLocation = glGetUniformLocation(shader.getid(), "view");
         glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
@@ -284,8 +302,19 @@ float vertices[] = {
 
         shader.set_float("mixValue", global_mix);
         glBindVertexArray(VAO1);
-        //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 0, 36);
+        for(int i = 0; i < 10; i++){
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::rotate(model, (float)(glfwGetTime() * rotateSpeed) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+            unsigned int modelLocation = glGetUniformLocation(shader.getid(), "model");
+            glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        }
 
         glfwPollEvents();
         glfwSwapBuffers(window);
